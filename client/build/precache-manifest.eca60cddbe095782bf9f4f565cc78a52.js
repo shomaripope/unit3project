bff5070455f73036b5c7d7a7af3d586f98b2a723
Shomari Pope <shomaripope@gmail.com>
2019-02-07 10:51:40 -0500

    committing recovered data

---
+++ client/build/precache-manifest.eca60cddbe095782bf9f4f565cc78a52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c6865533f35335e9ad3",
+    "url": "/static/css/main.608412b1.chunk.css"
+  },
+  {
+    "revision": "9c6865533f35335e9ad3",
+    "url": "/static/js/main.9c686553.chunk.js"
+  },
+  {
+    "revision": "0dddee6860b0e2273363",
+    "url": "/static/js/1.0dddee68.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a8e046dcbb29503cee2045c73522bfa3",
+    "url": "/index.html"
+  }
+];
